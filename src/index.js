ab508381ca523b784c48ed0c79dd60a2c00c7bbc
TedrosTK <tedydada@gmail.com>
2024-07-04 14:13:33 -0400

    BookInfo

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import {} from "@fortawesome/free-brands-svg-icons";
+// import {} from "@fortawesome/free-brands-svg-icons";
 import {
   faBars,
   faTimes,
